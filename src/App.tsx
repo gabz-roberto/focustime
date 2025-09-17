3aea4c048ea1182ca505e304b282d827878da210
Gabriel <44988043+gabz-roberto@users.noreply.github.com>
2025-09-17 12:27:37 -0300

    files recreated

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+        tempore ad animi accusantium dicta ea cupiditate unde expedita illum
+        repudiandae? Eius cupiditate maiores deleniti qui molestiae, animi
+        accusantium voluptas explicabo.
       </p>
     </>
   );
-}
+};
 
-export default App;
+export { App };
