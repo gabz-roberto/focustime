3176854b36a1fc6f8564600432b6c0c371584302
Gabriel <44988043+gabz-roberto@users.noreply.github.com>
2025-10-15 19:11:09 -0300

    state  creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Home from "./pages/Home";
 
 import "./styles/global.css";
 import "./styles/theme.css";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCicle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 const App = () => {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 };
 
 export { App };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,16 @@ import { MainTemplate } from "../../templates/MainTemplate";
 import { Container } from "../../components/Container";
 import { Countdown } from "../../components/Countdown";
 import MainForm from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
-const Home = () => {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+const Home = (props: HomeProps) => {
+  console.log(props);
+
   return (
     <MainTemplate>
       <Container>
