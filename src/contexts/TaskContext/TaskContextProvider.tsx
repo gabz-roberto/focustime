0472b28015d8bad77f1ea74987aac240f442d459
Gabriel <44988043+gabz-roberto@users.noreply.github.com>
2025-10-22 07:04:10 -0300

    reducer convert

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 
@@ -9,13 +9,59 @@ type TaskContextProviderProps = {
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  type ActionType = {
+    type: string;
+    payload?: number;
+  };
+
+  const [myState, dispatch] = useReducer(
+    (state, action: ActionType) => {
+      console.log(state, action);
+
+      switch (action.type) {
+        case "INCREMENT": {
+          if (!action.payload) return state;
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining + action.payload,
+          };
+        }
+        case "DECREMENT": {
+          if (!action.payload) return state;
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining - action.payload,
+          };
+        }
+        case "RESET": {
+          return {
+            secondsRemaining: 0,
+          };
+        }
+      }
+
+      return state;
+    },
+    {
+      secondsRemaining: 0,
+    },
+  );
+
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      {/* {children} */}
+      <h1>O número é: {JSON.stringify(myState)}</h1>
+      <button onClick={() => dispatch({ type: "DECREMENT", payload: 5 })}>
+        - 5
+      </button>
+      <button onClick={() => dispatch({ type: "INCREMENT", payload: 10 })}>
+        + 10
+      </button>
+      <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
     </TaskContext.Provider>
   );
 };
