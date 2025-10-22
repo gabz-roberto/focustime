79d3a0e2b1835a32c809e9013b58836e2d5453bd
Gabriel <44988043+gabz-roberto@users.noreply.github.com>
2025-10-22 07:24:27 -0300

    reducer refactoring

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 
@@ -9,59 +9,13 @@ type TaskContextProviderProps = {
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [state, setState] = useState(initialTaskState);
 
-  type ActionType = {
-    type: string;
-    payload?: number;
-  };
-
-  const [myState, dispatch] = useReducer(
-    (state, action: ActionType) => {
-      console.log(state, action);
-
-      switch (action.type) {
-        case "INCREMENT": {
-          if (!action.payload) return state;
-          return {
-            ...state,
-            secondsRemaining: state.secondsRemaining + action.payload,
-          };
-        }
-        case "DECREMENT": {
-          if (!action.payload) return state;
-          return {
-            ...state,
-            secondsRemaining: state.secondsRemaining - action.payload,
-          };
-        }
-        case "RESET": {
-          return {
-            secondsRemaining: 0,
-          };
-        }
-      }
-
-      return state;
-    },
-    {
-      secondsRemaining: 0,
-    },
-  );
-
-  // useEffect(() => {
-  //   console.log(state);
-  // }, [state]);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {/* {children} */}
-      <h1>O número é: {JSON.stringify(myState)}</h1>
-      <button onClick={() => dispatch({ type: "DECREMENT", payload: 5 })}>
-        - 5
-      </button>
-      <button onClick={() => dispatch({ type: "INCREMENT", payload: 10 })}>
-        + 10
-      </button>
-      <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+      {children}
     </TaskContext.Provider>
   );
 };
---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,6 @@
+// useReducer <- hook do React que recebe um reducer e u estado inicial
+// reducer <- função que recbe um estado atual e uma ação, e retorna o novo estado
+// state <- o estado atual
+// action <- a ação disparada , geralmente é um objeto com type e (opcionalmente) um payload
+// type <- o tipo da ação, geralmente uma string (podendo ser um enum, constante, etc)
+// payload <- os dados extras enviados junto a action, se necessário para atualizar o estado
